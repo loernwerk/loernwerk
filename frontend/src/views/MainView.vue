3f5ac476b51dae23f27ac001633f6353d51235d5
Alexander Vogt <alexander_vogt@outlook.de>
2023-06-27 17:07:31 +0200

    finish frontend setup

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="absolute top-0 bottom-0 left-0 right-0 flex flex-col">
-    <div
-      class="relative top-0 left-0 bottom-0 right-0 p-5 flex flex-grow"
-    ></div>
+  <div>
+    <!-- Empty div so eslint is happy -->
   </div>
+  >
 </template>
 
 <script setup lang="ts"></script>
