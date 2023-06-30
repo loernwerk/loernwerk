dbfd0bf9fdb59990f326df338dccba358cf1d675
Alexander Vogt <alexander_vogt@outlook.de>
2023-06-30 13:29:54 +0200

    smoother transition

--- frontend/src/views/LogInView.vue
+++ frontend/src/views/LogInView.vue
@@ -1,50 +1,53 @@
 <template>
-  <ContainerComponent class="w-1/2 mx-auto my-auto h-fit">
-    <template #Header>
-      <h1 class="underline text-xl">Anmelden:</h1>
-    </template>
-    <template #default>
-      <table class="w-full">
-        <tr>
-          <td class="p-1">Nutzername/Email:</td>
-          <td class="w-full p-1">
-            <TextInputComponent
-              placeHolder="Nutzername/Email"
-              @input-changed="(val) => (mailField = val)"
+  <div>
+    <img src="../assets/Logo.png" class="w-1/3 mx-auto" />
+    <ContainerComponent class="w-1/2 mx-auto my-auto h-fit">
+      <template #Header>
+        <h1 class="underline text-xl">Anmelden:</h1>
+      </template>
+      <template #default>
+        <table class="w-full">
+          <tr>
+            <td class="p-1">Nutzername/Email:</td>
+            <td class="w-full p-1">
+              <TextInputComponent
+                placeHolder="Nutzername/Email"
+                @input-changed="(val) => (mailField = val)"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td class="p-1">Passwort:</td>
+            <td class="w-full p-1">
+              <TextInputComponent
+                :hidden="true"
+                placeHolder="Passwort"
+                @input-changed="(val) => (passwordField = val)"
+              />
+            </td>
+          </tr>
+        </table>
+        <div class="flex items-center pt-4">
+          <div class="space-x-2">
+            <input
+              type="checkbox"
+              class="cursor-pointer"
+              v-model="keepLoggedIn"
             />
-          </td>
-        </tr>
-        <tr>
-          <td class="p-1">Passwort:</td>
-          <td class="w-full p-1">
-            <TextInputComponent
-              :hidden="true"
-              placeHolder="Passwort"
-              @input-changed="(val) => (passwordField = val)"
-            />
-          </td>
-        </tr>
-      </table>
-      <div class="flex items-center pt-4">
-        <div class="space-x-2">
-          <input
-            type="checkbox"
-            class="cursor-pointer"
-            v-model="keepLoggedIn"
-          />
-          <label class="cursor-pointer" @click="keepLoggedIn = !keepLoggedIn"
-            >Angemeldet bleiben</label
-          >
+            <label class="cursor-pointer" @click="keepLoggedIn = !keepLoggedIn"
+              >Angemeldet bleiben</label
+            >
+          </div>
+          <div class="flex-grow">
+            <!-- Empty div as space filler -->
+          </div>
+          <ButtonComponent class="w-fit" @click="checkLogIn()">
+            Anmelden
+          </ButtonComponent>
         </div>
-        <div class="flex-grow">
-          <!-- Empty div as space filler -->
-        </div>
-        <ButtonComponent class="w-fit" @click="checkLogIn()">
-          Anmelden
-        </ButtonComponent>
-      </div>
-    </template>
-  </ContainerComponent>
+      </template>
+    </ContainerComponent>
+  </div>
 </template>
 
 <script setup lang="ts">
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,11 +1,15 @@
 <!-- View for entering code -->
 <template>
   <div class="flex flex-col">
-    <img src="../assets/Logo.png" class="w-1/3 mx-auto my-5" />
-    <div class="mx-auto space-y-2">
-      <h1 class="text-3xl text-center">Code eingeben:</h1>
-      <CodeInput @code-entered="(code) => console.log(code)" />
-    </div>
+    <img src="../assets/Logo.png" class="w-1/3 mx-auto" />
+    <ContainerComponent class="mx-auto space-y-2 px-10 py-3">
+      <template #Header>
+        <h1 class="text-3xl text-center">Code eingeben:</h1>
+      </template>
+      <template #default>
+        <CodeInput @code-entered="(code) => console.log(code)" />
+      </template>
+    </ContainerComponent>
     <ButtonComponent
       class="absolute right-5 bottom-5 h-fit"
       @click="router.push('LogIn')"
@@ -17,6 +21,7 @@
 
 <script setup lang="ts">
 import ButtonComponent from '../components/ButtonComponent.vue';
+import ContainerComponent from '../components/ContainerComponent.vue';
 import CodeInput from '../components/CodeInput.vue';
 import { router } from '../router';
 </script>
