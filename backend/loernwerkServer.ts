d06379ad89c3786ffc878513b4f14be6fae08ccf
Alisa <Alisa.Hummel@gmx.de>
2023-07-02 16:27:01 +0200

    Build SequenceRouter

--- backend/loernwerkServer.ts
+++ backend/loernwerkServer.ts
@@ -9,6 +9,7 @@ import history from 'connect-history-api-fallback';
 import { readFile } from 'fs/promises';
 import { createServer } from 'https';
 import { AccountRouterFactory } from './router/AccountRouterFactory';
+import { SequenceRouterFactory } from './router/SequenceRouterFactory';
 
 /**
  * Main class and entrypoint of the backend server.
@@ -56,6 +57,7 @@ class loernwerkServer {
 
         // Setting up routers, TODO
         app.use('/api/account', new AccountRouterFactory().buildRouter());
+        app.use('/api/sequence', new SequenceRouterFactory().buildRouter());
 
         // Serving built vue app
         app.use(history());
@@ -69,13 +71,13 @@ class loernwerkServer {
             process.exit(0);
         });
 
-        const port = parseInt(process.env.PORT) || 5000;
+        const port = parseInt(process.env.PORT as string) || 5000;
         const hostname = process.env.HOSTNAME || 'localhost';
 
         const caFile = process.env.SSL_CAFILE;
         const keyFile = process.env.SSL_KEYFILE;
         const certFile = process.env.SSL_CERTFILE;
-        const sslPort = parseInt(process.env.SSL_PORT) || 5443;
+        const sslPort = parseInt(process.env.SSL_PORT as string) || 5443;
 
         // Starting the server
         if (process.env.DISABLE_HTTP === undefined) {
