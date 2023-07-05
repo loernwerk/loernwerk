243f01813dfdc7d9e7c8d9bdb8be88c9bb6f98d0
Alisa <Alisa.Hummel@gmx.de>
2023-07-05 15:47:51 +0200

    Spinner in ButtonComponent

--- frontend/src/components/ButtonComponent.vue
+++ frontend/src/components/ButtonComponent.vue
@@ -1,13 +1,39 @@
 <!-- Button to interact with -->
 <template>
-  <InteractableComponent @click="(e) => $emit('click', e)">
-    <!-- Slot for the text of the button -->
-    <slot></slot>
-  </InteractableComponent>
+  <div>
+    <div class="relative">
+      <div class="absolute w-full h-full text-center" v-if="loading">
+        <div
+          class="absolute h-full w-full bg-gray-300 opacity-70 rounded-md"
+        ></div>
+        <FontAwesomeIcon icon="spinner" size="1x" class="animate-spin mt-2" />
+      </div>
+      <InteractableComponent @click="(e) => $emit('click', e)">
+        <!-- Slot for the text of the button -->
+        <slot></slot>
+      </InteractableComponent>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import InteractableComponent from './InteractableComponent.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSpinner);
+
+defineProps({
+  /**
+   * Whether the button displays a loading spinner
+   */
+  loading: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
 
 defineEmits([
   /**
