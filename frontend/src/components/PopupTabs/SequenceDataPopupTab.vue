e349f7438dc8840afb0a5a7af669fa899350dea7
Alexey Simakov <alexey.d.simakov@gmail.com>
2023-07-16 13:01:05 +0200

    further changes

---
+++ frontend/src/components/PopupTabs/SequenceDataPopupTab.vue
@@ -0,0 +1,21 @@
+<template>
+  <ContainerComponent>
+  <h3>Der Code der ausgewählten Sequenz ist : {{ sequenceData.code }}</h3>
+  <h3>Der Link zur momentan ausgewählten Sequenz ist: {{ link }}</h3>
+  </ContainerComponent>
+</template>
+
+<script setup lang="ts">
+import {toRaw, watch} from "vue";
+import useEventsBus from "../../eventBus";
+import {SequenceRestInterface} from "../../restInterfaces/SequenceRestInterface";
+import ContainerComponent from "../ContainerComponent.vue";
+const { bus } = useEventsBus();
+let sequenceData;
+//let link = SequenceRestInterface.getSequence(sequenceData.code);
+let link = 'someUrl';
+watch(()=>bus.value.get('sequence'), (sequence) => {
+  sequenceData = toRaw(sequence)[0];
+});
+
+</script>
--- frontend/src/views/SequenceOverviewView.vue
+++ frontend/src/views/SequenceOverviewView.vue
@@ -1,47 +1,47 @@
 <template>
-  <main>
+  <div class="absolute top-0 bottom-0 left-0 right-0 flex flex-col">
     <PopupComponent>
       <TabbedContainer :tabs='tabNames'>
         <template v-slot:[0]="{ item }" >
-          <TagSequencePopupTab
-          />
+          <TagSequencePopupTab/>
         </template>
         <template v-slot:[1]="{ item }" >
-          <DeleteSequencePopup
-          />
+          <DeleteSequencePopup/>
         </template>
         <template v-slot:[2]="{ item }" >
           <ShareSequencePopupTab/>
         </template>
+        <template v-slot:[3]="{ item }" >
+          <SequenceDataPopupTab/>
+        </template>
       </TabbedContainer>
     </PopupComponent>
-  <div class="w-full h-full">
-    <div class="h-24.5 col-span-2 w-full -mt-28 -mb-20 flex flex-row">
-      <div><img src="../assets/Logo.png" class="w-1/3 -ml-44 scale-50" /></div>
-      <div>
-        <font-awesome-icon
-          :icon="['fas', 'circle-user']"
-          size="2xl"
-          style="color: #22511f"
-          class="mr-12 -mb-40 scale-150"
-          @click="router.push('LogIn')"
-        />
-      </div>
+  <div class="w-full h-full pt-[60px]">
+    <div class="h-[80px]">
+      <!--navbar-->
+    </div>
+    <div class="absolute right-2.5 top-[96px]">
+      <SearchBarComponent class="mb-2"></SearchBarComponent>
     </div>
 
-    <div class="columns-2" style="height: 85%">
-      <SequenceDisplayContainer
-        :name="name"
-        :sequences="sequences"
-      ></SequenceDisplayContainer>
-      <SequenceDisplayContainer
-        :name="name2"
-        :sequences="sharedSequences"
-        userId="originalUser"
-      ></SequenceDisplayContainer>
+    <div class="flex pt-5 w-full h-full" style="height: 90%">
+      <div class="flex-1 mr-1 ml-2">
+        <SequenceDisplayContainer
+            :name="name"
+            :sequences="sequences"
+        >
+        </SequenceDisplayContainer>
+      </div>
+      <div class="flex-1 ml-1 mr-2">
+        <SequenceDisplayContainer
+            :name="name2"
+            :sequences="sharedSequences"
+            userId="originalUser"
+        ></SequenceDisplayContainer>
+      </div>
     </div>
   </div>
-  </main>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -52,41 +52,39 @@ import {
   LoernwerkErrorCodes,
 } from '../../../backend/loernwerkError';
 import { ISequence } from '../../../model/sequence/ISequence';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
 import ButtonComponent from '../components/ButtonComponent.vue';
 import PopupComponent from "../components/PopupComponent.vue";
 import TabbedContainer from "../components/TabbedContainer.vue";
 import TagSequencePopupTab from "../components/PopupTabs/TagSequencePopupTab.vue";
 import DeleteSequencePopup from "../components/PopupTabs/DeleteSequencePopup.vue";
 import ShareSequencePopupTab from "../components/PopupTabs/ShareSequencePopupTab.vue";
+import SearchBarComponent from "../components/SearchBarComponent.vue";
+import SequenceDataPopupTab from "../components/PopupTabs/SequenceDataPopupTab.vue";
 const name = 'Meine Sequenzen:';
 const name2 = 'Mit mir geteilte Sequenzen:';
 let sequences, sharedSequences: ISequence[];
-library.add(faCircleUser);
 
 let originalUser: unknown;
-const tabs = ['TagSequencePopupTab', 'DeleteSequencePopup','ShareSequencePopupTab']
-const tabNames = ['Sequenz Taggen','Sequenz löschen','Sequenz mit Lehrkräften teilen']
+const tabs = ['TagSequencePopupTab', 'DeleteSequencePopup','ShareSequencePopupTab','SequenceDataPopupTab']
+const tabNames = ['Sequenz Taggen','Sequenz löschen','Sequenz mit Lehrkräften teilen','Sequenz mit Teilnehmern teilen']
 
-let items = [
-  {
-    id: '1',
-    name: 'Sequenz löschen',
-    component: ButtonComponent
-  },
-  {
-    id: '2',
-    name: 'Sequenz teilen',
-    component: ButtonComponent
-  },
-  {
-    id: '3',
-    name: 'Sequenz Taggen',
-    component: ButtonComponent
-  }
-]
+// let items = [
+//   {
+//     id: '1',
+//     name: 'Sequenz löschen',
+//     component: ButtonComponent
+//   },
+//   {
+//     id: '2',
+//     name: 'Sequenz teilen',
+//     component: ButtonComponent
+//   },
+//   {
+//     id: '3',
+//     name: 'Sequenz Taggen',
+//     component: ButtonComponent
+//   }
+// ]
 //mocking a user with two sequences, zero shared
 try {
   originalUser = 'John Doe'; //await AccountRestInterface.getOwnAccount();
