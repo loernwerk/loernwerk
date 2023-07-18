bd5f03a3a5a07ed8039cd816e60acb0b0279d530
Alexander Vogt <alexander_vogt@outlook.de>
2023-07-18 10:06:11 +0200

    base names

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,8 +1,20 @@
 import { createI18n } from 'vue-i18n';
 
 const messages = {
-  de: {},
-  en: {},
+  de: {
+    message: {
+      save: 'Speichern',
+      cancel: 'Abbrechen',
+      delete: 'Löschen',
+    },
+  },
+  en: {
+    message: {
+      save: 'Save',
+      cancel: 'Cancel',
+      delete: 'Delete',
+    },
+  },
 };
 
 export const i18n = createI18n({
