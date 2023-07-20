4be9bc0b8d35617dc687d68ce689e52fe78f5957
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2023-07-20 20:07:14 +0200

    added doc

--- model/configuration/RegistrationType.ts
+++ model/configuration/RegistrationType.ts
@@ -1,5 +1,17 @@
+/**
+ * Holds the registration types
+ */
 export enum RegistrationType {
+    /**
+     * Everyone can sign up
+     */
     OPEN = 'open',
+    /**
+     * Sign up is possible through a invite code
+     */
     INVITE = 'invite',
+    /**
+     * only admins can create codes
+     */
     CLOSED = 'closed',
 }
