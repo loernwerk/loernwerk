031875c96196ce9f517e90c9203c0f896bc0a843
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2023-07-20 19:11:27 +0200

    compacted a bit and make lint happy hopefully (4)

--- model/configuration/ConfigType.ts
+++ model/configuration/ConfigType.ts
@@ -2,34 +2,31 @@
  * Saves the type of a value for a certain configuration setting.
  */
 export type ConfigType =
+    {
+        /**
+         * Type of the configuration setting
+         */
+        type: 'string' | 'boolean' | 'codes';
+    }
     | {
-          /**
-           * Type of the configuration setting
-           */
-          type: 'string' | 'boolean';
-      }
+        /**
+         * Type of the configuration setting
+         */
+        type: 'number';
+        /**
+         * Whether the number option is unlimited or not
+         */
+        options: NumberOption;
+    }
     | {
-          /**
-           * Type of the configuration setting
-           */
-          type: 'number';
-          /**
-           * Whether the number option is unlimited or not
-           */
-          options: NumberOption;
-      }
-    | {
-          /**
-           * Type of the configuration setting
-           */
-          type: 'enum';
-          /**
-           * All possible options for this enum
-           */
-          options: string[];
-      }
-    | {
-          type: 'codes';
-      };
+        /**
+         * Type of the configuration setting
+         */
+        type: 'enum';
+        /**
+         * All possible options for this enum
+         */
+        options: string[];
+    };
 
 export type NumberOption = 'limited' | 'unlimited';
