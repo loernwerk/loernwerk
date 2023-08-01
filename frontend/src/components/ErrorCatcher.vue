d1752f20ff92a7a3f92ed1bf37cbd98388f17483
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2023-08-01 13:43:43 +0200

    beginning of toasts

--- frontend/src/components/ErrorCatcher.vue
+++ frontend/src/components/ErrorCatcher.vue
@@ -1,11 +1,27 @@
 <template>
   <slot></slot>
+  <div class="relative">
+    <div class="absolute bottom-0 right-0">
+      <li>
+        <ul>
+          Test
+        </ul>
+        <ul v-for="mes of errormessages" :key="mes">
+          mes
+        </ul>
+        >
+      </li>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue';
+import { onErrorCaptured, ref, Ref } from 'vue';
+
+const errormessages: Ref<Array<string>> = ref([]);
 
 onErrorCaptured((err, instance, info) => {
   void instance;
+  errormessages.value.concat(err.message);
   console.log('this needed to be displayed, err:' + err + ' info: ' + info);
   return false;
 });
