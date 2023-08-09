253bf7b65a23ebe099dbd8f6edf3d3c94246e30d
Alex | Kronox <alexander_vogt@outlook.de>
2023-08-09 17:04:19 +0200

    config rest interface

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
     test: {
-        include: ['tests/component/**.test.ts'],
+        include: ['tests/component/**/*.test.ts'],
         setupFiles: ['tests/component/translation_mock.setup.ts', 'tests/component/router_mock.setup.ts'],
         globals: true,
         environment: 'happy-dom',
