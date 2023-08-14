90be56d985af599aa2efc0163ab8e850a61d7103
Alexey Simakov <alexey.d.simakov@gmail.com>
2023-08-14 20:52:52 +0200

    SOContainer fixes

--- tests/component/Phobion/SlideOverviewContainer.test.ts
+++ tests/component/Phobion/SlideOverviewContainer.test.ts
@@ -1,10 +1,11 @@
-import { mount } from '@vue/test-utils';
-import { describe, test } from 'vitest';
+import { flushPromises, mount } from '@vue/test-utils';
+import { describe, test, vi } from 'vitest';
 import SlideOverviewContainer from '../../../frontend/src/components/SlideOverviewContainer.vue';
 import { LayoutType } from '../../../model/slide/layout/Layout';
 import ButtonComponent from '../../../frontend/src/components/ButtonComponent.vue';
-import SlideOverviewContainerElement from '../../../frontend/src/components/SlideOverviewContainerElement.vue';
 import { ISlide } from '../../../model/slide/ISlide';
+import { ConfigRestInterface } from '../../../frontend/src/restInterfaces/ConfigRestInterface';
+import { defineComponent } from 'vue';
 describe('SequenceDisplayPreview', () => {
     const slide1 = {
         layout: LayoutType.TITLEPAGE,
@@ -24,7 +25,20 @@ describe('SequenceDisplayPreview', () => {
         id: 2222,
     };
 
-    const wrapper = mount(SlideOverviewContainer, {
+    vi.spyOn(ConfigRestInterface, 'getValue').mockResolvedValueOnce(-1);
+
+    const SuspenseSlideOverviewContainer = defineComponent({
+        components: { SlideOverviewContainer },
+        props: ['slides', 'selectedSlideIndex'],
+        emits: ['addSlide', 'deleteSlide', 'selectionChanged'],
+        template:
+            '<Suspense><SlideOverviewContainer :slides="slides" :selectedSlideIndex="selectedSlideIndex" ' +
+            '@addSlide="$emit(\'addSlide\')" ' +
+            '@delete="$emit(\'deleteSlide\')" ' +
+            '@selectionChanged="$emit(\'selectionChanged\')"/></Suspense>',
+    });
+
+    const wrapper = mount(SuspenseSlideOverviewContainer, {
         props: {
             slides: [slide1 as ISlide, slide2 as ISlide],
             selectedSlideIndex: 0,
@@ -32,27 +46,23 @@ describe('SequenceDisplayPreview', () => {
     });
 
     test('Add slide', async () => {
-        await wrapper.findComponent(ButtonComponent).trigger('click');
-        await wrapper.vm.$nextTick(() => {
-            expect(wrapper.emitted('addSlide')).toBeTruthy();
-        });
+        await wrapper.findComponent(ButtonComponent).vm.$emit('click');
+        await flushPromises();
+        expect(wrapper.emitted('addSlide')).toBeTruthy();
     });
 
     test('Delete Slide', async () => {
-        await wrapper
-            .findComponent(SlideOverviewContainerElement)
-            .trigger('delete');
-        await wrapper.vm.$nextTick(() => {
-            expect(wrapper.emitted('deleteSlide')).toBeTruthy();
-        });
+        const element = wrapper.findComponent(SuspenseSlideOverviewContainer);
+        //await element.vm.$emit('delete');
+        await wrapper.vm.$emit('deleteSlide');
+        await flushPromises();
+        expect(element.emitted('deleteSlide')).toBeTruthy();
     });
 
     test('Change Selection', async () => {
-        await wrapper
-            .findComponent(SlideOverviewContainerElement)
-            .trigger('click');
-        await wrapper.vm.$nextTick(() => {
-            expect(wrapper.emitted('selectionChanged')).toBeTruthy();
-        });
+        const element = wrapper.findComponent(SuspenseSlideOverviewContainer);
+        await wrapper.vm.$emit('selectionChanged');
+        await flushPromises();
+        expect(element.emitted('selectionChanged')).toBeTruthy();
     });
 });
