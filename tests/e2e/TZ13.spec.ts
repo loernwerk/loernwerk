f07e36ad09325adab26aa0e9a89185fde4f20f7e
Moritz R <c0derMo@users.noreply.github.com>
2023-08-17 01:02:28 +0200

    TZ13

---
+++ tests/e2e/TZ13.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('TZ13', async ({ page, browserName, context }) => {
+    await context.clearCookies();
+    await page.goto('./');
+    await page.getByText('Anmelden').click();
+
+    expect(page).toHaveURL(/.*login/);
+    await page.getByPlaceholder('Benutzername/E-Mail').click();
+    await page.getByPlaceholder('Benutzername/E-Mail').fill('admin');
+    await page.getByPlaceholder('Passwort').click();
+    await page.getByPlaceholder('Passwort').fill('12345678');
+    await page.getByText('Anmelden', { exact: true }).click();
+    expect(page).toHaveURL(/.*overview/);
+
+    await page.getByRole('link', { name: 'Admin' }).click();
+    expect(page).toHaveURL(/.*admin/);
+
+    const username = `TZ12-13-tobeedited-tobedeleted-${browserName}`;
+
+    await page.getByText(username).click();
+    await page.getByText('Löschen').click();
+    expect(
+        await page.getByText(/.*Benutzer erstellen.*/).allInnerTexts()
+    ).not.toContain(username);
+});
