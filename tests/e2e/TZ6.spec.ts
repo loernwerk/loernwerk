fde83be0a6e7e1c17a315d4b9fd309b9e1407215
Alisa <Alisa.Hummel@gmx.de>
2023-08-17 22:32:21 +0200

    test TZ6

---
+++ tests/e2e/TZ6.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('TZ6', async ({ page }) => {
+    await page.goto('./');
+    await page.getByText('Anmelden').click();
+    await expect(page).toHaveURL(/.*login/);
+
+    await page.getByPlaceholder('Benutzername/E-Mail').click();
+    await page.getByPlaceholder('Benutzername/E-Mail').fill('lehrkraft');
+    await page.getByPlaceholder('Passwort').click();
+    await page.getByPlaceholder('Passwort').fill('12345678');
+    await page.getByText('Anmelden', { exact: true }).click();
+    await expect(page).toHaveURL(/.*overview/);
+
+    await page
+        .locator(
+            'div:nth-child(2) > div:nth-child(2) > div > .mt-2 > div > div > div:nth-child(2) > div > .space-y-2 > div:nth-child(3) > .flex-1 > .relative > .p-2 > div'
+        )
+        .first()
+        .click();
+    await expect(page).toHaveURL(/.*edit.*/);
+
+    await page.getByText('Speichern').click();
+    await expect(page).toHaveURL(/.*overview/);
+
+    await page
+        .locator(
+            'div:nth-child(2) > div:nth-child(2) > div > .mt-2 > div > div > div:nth-child(2) > div > .space-y-2 > div:nth-child(3) > div:nth-child(2) > .relative > .p-2 > div > .svg-inline--fa > path'
+        )
+        .click();
+    const element = page.getByText('Mit Teilnehmenden teilen');
+    expect(element !== undefined).toBeTruthy();
+});
