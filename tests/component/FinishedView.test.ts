9164d0c915eb66165619ffa6ac24b84f9feb718e
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2023-08-18 16:50:56 +0200

    finished slideview

--- tests/component/FinishedView.test.ts
+++ tests/component/FinishedView.test.ts
@@ -1,99 +1,90 @@
-import { defineComponent } from "vue";
-import FinishedView from "../../frontend/src/views/FinishedView.vue"
-import ButtonComponent from "../../frontend/src/components/ButtonComponent.vue"
+import { defineComponent } from 'vue';
+import FinishedView from '../../frontend/src/views/FinishedView.vue';
+import ButtonComponent from '../../frontend/src/components/ButtonComponent.vue';
 import { SequenceRestInterface } from '../../frontend/src/restInterfaces/SequenceRestInterface';
-import { flushPromises, mount, shallowMount } from '@vue/test-utils';
+import { flushPromises, mount } from '@vue/test-utils';
 import { describe, test, vi } from 'vitest';
-import { routerMock } from './router_mock.setup';
-import { G } from "vitest/dist/types-198fd1d9";
 
-describe("FinishedView", () => {
-
+describe('FinishedView', () => {
     const SuspenseFinishedView = defineComponent({
         components: { FinishedView },
         template: '<Suspense><FinishedView :sequenceCode="code" /></Suspense>',
         props: {
-            code: String
-        }
+            code: String,
+        },
     });
 
-    const test2 = defineComponent({
-        components: { ButtonComponent },
-        template: '<ButtonComponent class="w-fit" @click="downloadCertificate()">  $t(finished.certificate) </ButtonComponent>'
-    });
-    
-    test("correct Sequence fetch call", async () => {
-        const getMetadata = vi.spyOn(SequenceRestInterface, "getMetadataForStudent")
+    test('correct Sequence fetch call', async () => {
+        const getMetadata = vi.spyOn(
+            SequenceRestInterface,
+            'getMetadataForStudent'
+        );
 
         getMetadata.mockResolvedValueOnce({
-            name: "test",
-            code: "123456",
+            name: 'test',
+            code: '123456',
             creationDate: new Date(),
             modificationDate: new Date(),
             authorId: 1,
             writeAccess: [],
             readAccess: [],
             tags: [],
-            slideCount: 1
-        })
+            slideCount: 1,
+        });
 
-        const wrapper = mount(SuspenseFinishedView, {
+        mount(SuspenseFinishedView, {
             props: {
-                code: "123456"
-            }
-        })
+                code: '123456',
+            },
+        });
 
-        await flushPromises()
+        await flushPromises();
 
-        expect(getMetadata).toBeCalledTimes(1)
-        expect(getMetadata).toBeCalledWith("123456")
+        expect(getMetadata).toBeCalledTimes(1);
+        expect(getMetadata).toBeCalledWith('123456');
+    });
 
-    })
-
-    test("called getcertificateurl with correct params", async () => {
-        const getMetadata = vi.spyOn(SequenceRestInterface, "getMetadataForStudent")
-        const getUrl = vi.spyOn(SequenceRestInterface, "getUrlForCertificate")
-        const windowopen = vi.spyOn(window, "open")
+    test('called getcertificateurl with correct params', async () => {
+        const getMetadata = vi.spyOn(
+            SequenceRestInterface,
+            'getMetadataForStudent'
+        );
+        const getUrl = vi.spyOn(SequenceRestInterface, 'getUrlForCertificate');
+        const windowopen = vi.spyOn(window, 'open');
 
         getMetadata.mockResolvedValueOnce({
-            name: "test",
-            code: "123456",
+            name: 'test',
+            code: '123456',
             creationDate: new Date(),
             modificationDate: new Date(),
             authorId: 1,
             writeAccess: [],
             readAccess: [],
             tags: [],
-            slideCount: 1
-        })
+            slideCount: 1,
+        });
 
-        getUrl.mockResolvedValueOnce(
-            "test.de"
-        )
-
+        getUrl.mockResolvedValueOnce('test.de');
 
         const wrapper = mount(SuspenseFinishedView, {
             props: {
-                code: "123456"
-            }
-        })
+                code: '123456',
+            },
+        });
 
-        await flushPromises()
+        await flushPromises();
 
-        expect(getMetadata).toBeCalledTimes(1)
-        expect(getMetadata).toBeCalledWith("123456")
+        expect(getMetadata).toBeCalledTimes(1);
+        expect(getMetadata).toBeCalledWith('123456');
 
-        await wrapper.getComponent(ButtonComponent).vm.$emit("click")
+        await wrapper.getComponent(ButtonComponent).vm.$emit('click');
 
-        await flushPromises()
+        await flushPromises();
 
-        expect(getUrl).toBeCalled(1)
-        expect(getUrl).toBeCalledWith("123456", "de")
+        expect(getUrl).toBeCalled(1);
+        expect(getUrl).toBeCalledWith('123456', 'de');
 
-        expect(windowopen).toBeCalled(1)
+        expect(windowopen).toBeCalled(1);
         //not testing the argument, because it is a promise, but since it is what we return in the geturl function it'll work
-
-    })
-
-
-})
+    });
+});
--- tests/component/SlideView.test.ts
+++ tests/component/SlideView.test.ts
@@ -1,92 +1,140 @@
-import { defineComponent } from "vue";
-import SlideView from "../../frontend/src/views/SlideView.vue"
+import { defineComponent } from 'vue';
+import SlideView from '../../frontend/src/views/SlideView.vue';
 import { SequenceRestInterface } from '../../frontend/src/restInterfaces/SequenceRestInterface';
+import ButtonComponent from '../../frontend/src/components/ButtonComponent.vue';
 import { flushPromises, mount } from '@vue/test-utils';
 import { describe, test, vi } from 'vitest';
 import { routerMock } from './router_mock.setup';
+import { LayoutType } from '../../model/slide/layout/Layout';
 
-describe("SlideView", () => {
-
+describe('SlideView', () => {
     const SuspenseSlideView = defineComponent({
         components: { SlideView },
         template: '<Suspense><SlideView :sequenceCode="code" /></Suspense>',
         props: {
-            code: String
-        }
+            code: String,
+        },
     });
 
-    test("correct Sequence fetch call", async () => {
-        const getMetadata = vi.spyOn(SequenceRestInterface, "getMetadataForStudent")
-        getMetadata.mockImplementation( (code) => {
-            return new Promise(() => {
-                return {
-                    name: "test",
-                    code: code,
-                    creationDate: new Date(),
-                    modificationDate: new Date(),
-                    authorId: 1,
-                    writeAccess: [],
-                    readAccess: [],
-                    tags: [],
-                    slideCount: 1
-                }
-            })
-        })
+    test('correct Sequence fetch call', async () => {
+        const getMetadata = vi.spyOn(
+            SequenceRestInterface,
+            'getMetadataForStudent'
+        );
+        getMetadata.mockResolvedValueOnce({
+            name: 'test',
+            code: '123456',
+            creationDate: new Date(),
+            modificationDate: new Date(),
+            authorId: 1,
+            writeAccess: [],
+            readAccess: [],
+            tags: [],
+            slideCount: 1,
+        });
         const wrapper = mount(SuspenseSlideView, {
             props: {
-                code: "123456"
-            }
-        })
+                code: '123456',
+            },
+        });
 
-        await flushPromises()
+        void wrapper;
 
-        expect(getMetadata).toBeCalledTimes(1)
-        expect(getMetadata).toBeCalledWith("123456")
+        await flushPromises();
 
-    })
+        expect(getMetadata).toBeCalledTimes(1);
+        expect(getMetadata).toBeCalledWith('123456');
+    });
 
-    test("correct follow up calls", async () => {
-        const getMetadata = vi.spyOn(SequenceRestInterface, "getMetadataForStudent")
-        const getSlide = vi.spyOn(SequenceRestInterface, "getSlide")
+    test('correct follow up calls', async () => {
+        const getMetadata = vi.spyOn(
+            SequenceRestInterface,
+            'getMetadataForStudent'
+        );
+        const getSlide = vi.spyOn(SequenceRestInterface, 'getSlide');
 
-        getMetadata.mockImplementation( (code) => {
-            return new Promise(() => {
-                return {
-                    name: "test",
-                    code: code,
-                    creationDate: new Date(),
-                    modificationDate: new Date(),
-                    authorId: 1,
-                    writeAccess: [],
-                    readAccess: [],
-                    tags: [],
-                    slideCount: 2
-                }
-            })
-        })
+        getMetadata.mockResolvedValueOnce({
+            name: 'test',
+            code: '123456',
+            creationDate: new Date(),
+            modificationDate: new Date(),
+            authorId: 1,
+            writeAccess: [],
+            readAccess: [],
+            tags: [],
+            slideCount: 1,
+        });
 
-        getSlide.mockImplementation((sequenceCode:string, slideIndex:number) => {
-            return new Promise(() => {
-                return {}
-            })
-        })
+        getSlide.mockResolvedValueOnce({
+            layout: LayoutType.TITLEPAGE,
+            content: {},
+            backgroundColor: 'test',
+            sequenceCode: '0',
+            order: 0,
+            id: 12391,
+        });
+
+        mount(SuspenseSlideView, {
+            props: {
+                code: '123456',
+            },
+        });
+
+        await flushPromises();
+
+        expect(getMetadata).toBeCalledTimes(1);
+        expect(getMetadata).toBeCalledWith('123456');
+
+        expect(routerMock.push).toBeCalledTimes(0);
+
+        expect(getSlide).toBeCalledTimes(1);
+        expect(getSlide).toBeCalledWith('123456', 0);
+    });
+
+    test('correct router call', async () => {
+        const getMetadata = vi.spyOn(
+            SequenceRestInterface,
+            'getMetadataForStudent'
+        );
+        const getSlide = vi.spyOn(SequenceRestInterface, 'getSlide');
+
+        getMetadata.mockResolvedValueOnce({
+            name: 'test',
+            code: '123456',
+            creationDate: new Date(),
+            modificationDate: new Date(),
+            authorId: 1,
+            writeAccess: [],
+            readAccess: [],
+            tags: [],
+            slideCount: 1,
+        });
+
+        getSlide.mockResolvedValueOnce({
+            layout: LayoutType.TITLEPAGE,
+            content: {},
+            backgroundColor: 'test',
+            sequenceCode: '0',
+            order: 0,
+            id: 12391,
+        });
 
         const wrapper = mount(SuspenseSlideView, {
             props: {
-                code: "123456"
-            }
-        })
+                code: '123456',
+            },
+        });
 
-        await flushPromises()
+        await flushPromises();
 
-        expect(getMetadata).toBeCalledTimes(1)
-        expect(getMetadata).toBeCalledWith("123456")
+        expect(getMetadata).toBeCalledTimes(1);
+        expect(getMetadata).toBeCalledWith('123456');
 
-        expect(routerMock.push).toBeCalledTimes(0)
+        expect(getSlide).toBeCalledTimes(1);
+        expect(getSlide).toBeCalledWith('123456', 0);
 
-        expect(getSlide).toBeCalledTimes(1)
-        expect(getSlide).toBeCalledWith("123456", 0)
+        await wrapper.getComponent(ButtonComponent).vm.$emit('click');
 
-    })
-
-})
+        expect(routerMock.push).toBeCalledTimes(1);
+    });
+});
