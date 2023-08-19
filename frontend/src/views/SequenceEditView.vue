d719b1d9cace75f4aa7f168302bd02e1966a5941
Moritz R <c0derMo@users.noreply.github.com>
2023-08-19 12:31:11 +0200

    Fix deleting last slide if its active

--- frontend/src/views/SequenceEditView.vue
+++ frontend/src/views/SequenceEditView.vue
@@ -282,6 +282,9 @@ function deleteSlide(index: number): void {
   if (sequence.value.slides.length == 1) {
     return;
   }
+  if (index === sequence.value.slides.length - 1) {
+    selectedSlideIndex.value -= 1;
+  }
   sequence.value.slides.splice(index, 1);
   sequence.value.slideCount--;
 }
