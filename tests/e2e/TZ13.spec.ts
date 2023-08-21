6bcff1fec53f209345db2b2a8a1c88f4886a6dae
Moritz R <c0derMo@users.noreply.github.com>
2023-08-21 11:12:16 +0200

    Trying to improve

--- tests/e2e/TZ13.spec.ts
+++ tests/e2e/TZ13.spec.ts
@@ -24,9 +24,7 @@ test('TZ13', async ({ page, browserName, context }) => {
 
     const username = `TZ12-13-tobeedited-tobedeleted-${browserName}`;
 
-    await page
-        .locator('div:nth-child(4) > .p-0\\.5 > div > .relative > .p-2')
-        .click();
+    await page.getByText(username).click();
     await page.getByText('Löschen').click();
     expect(
         await page
