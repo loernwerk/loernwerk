9067c42927d877093c435223c83faab02f969b65
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2023-08-21 14:17:44 +0200

    first router tests

--- tests/unit/AccountRouter.test.ts
+++ tests/unit/AccountRouter.test.ts
@@ -1,16 +1,109 @@
 import { AccountRouterFactory } from '../../backend/router/AccountRouterFactory';
-import express from 'express';
+import { Router } from 'express';
+import { AccountController } from '../../backend/controller/AccountController';
+import { UserClass } from '../../model/user/IUser';
+import {
+    LoernwerkError,
+    LoernwerkErrorCodes,
+} from '../../model/loernwerkError';
 
-jest.mock('../../backend/controller/AccountController');
-const app = express();
-app.use('/api/account', new AccountRouterFactory().buildRouter());
+const sendStatusFn = jest.fn();
+/**
+ * Calls the handlefunction on the given router
+ * @param router the router to call on
+ * @param param params that can be an value assigned on
+ */
+function handleRouter(router: Router, param: RouterOption): void {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    router.handle(param, { sendStatus: sendStatusFn });
+}
 
-describe('AccountRouter', () => {
-    beforeEach(() => {
+describe('Accountrouter tests', () => {
+    afterEach(() => {
         jest.clearAllMocks();
     });
+    test('Logout', () => {
+        const router = new AccountRouterFactory().buildRouter();
 
-    test('correct login call', () => {
-        //leer
+        const destroyFn = jest.fn().mockImplementation((func) => {
+            func();
+        });
+
+        handleRouter(router, {
+            url: '/logout',
+            method: 'POST',
+            session: {
+                destroy: destroyFn,
+                userId: 80,
+            },
+        });
+
+        expect(destroyFn).toBeCalledTimes(1);
+        expect(sendStatusFn).toBeCalledWith(204);
+    });
+
+    test('incorrect login', () => {
+        const router = new AccountRouterFactory().buildRouter();
+        const loginFn = jest.spyOn(AccountController, 'tryLogin');
+        loginFn.mockImplementationOnce(() => {
+            throw new LoernwerkError(
+                'irgendwas',
+                LoernwerkErrorCodes.BAD_REQUEST
+            );
+        });
+        handleRouter(router, {
+            url: '/login',
+            method: 'post',
+            body: {
+                usernameOrEmail: 'test',
+                password: 'test',
+            },
+        });
+
+        expect(loginFn).toBeCalledTimes(1);
+        expect(loginFn).toBeCalledWith('test', 'test');
+        expect(sendStatusFn).toBeCalledWith(400);
+    });
+
+    test('login', () => {
+        //not working however
+        const router = new AccountRouterFactory().buildRouter();
+        const loginFn = jest.spyOn(AccountController, 'tryLogin');
+        loginFn.mockResolvedValueOnce({
+            name: 'test',
+            mail: 'test@test.de',
+            type: UserClass.ADMIN,
+            sharedSequencesReadAccess: [],
+            sharedSequencesWriteAccess: [],
+            password: 'test',
+            id: 1234,
+        });
+
+        handleRouter(router, {
+            url: '/login',
+            method: 'post',
+            body: {
+                usernameOrEmail: 'test',
+                password: 'test',
+                stayLoggedIn: false,
+            },
+        });
+
+        expect(loginFn).toBeCalledTimes(1);
+        expect(loginFn).toBeCalledWith('test', 'test');
+        expect(sendStatusFn).toBeCalledWith(204);
     });
 });
+
+interface RouterOption {
+    url?: string;
+    method?: string;
+    session?: {
+        userId?: number;
+        // eslint-disable-next-line
+        destroy?: any;
+    };
+    // eslint-disable-next-line
+    body?: any;
+}
